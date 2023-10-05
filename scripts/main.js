b53d15e0d1d3bf22714590fca924bb32c11be954
DariaCherr <116807318+DariaCherr@users.noreply.github.com>
2023-10-05 19:49:46 +0300

    version11

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
+const header = document.querySelector(".header");
+const mediaQuery = window.matchMedia('(max-width: 800px)');
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("burger").addEventListener("click", function() {
-        document.querySelector("header").classList.add("open");
+        header.classList.add("open");
     })
     document.getElementById("burger-close").addEventListener("click", function() {
-        document.querySelector("header").classList.remove("open");
+        header.classList.remove("open");
     })
 })
