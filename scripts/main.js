4712542707198028d6ad842fca80556b2dd2dc30
DariaCherr <116807318+DariaCherr@users.noreply.github.com>
2023-10-06 13:51:35 +0300

    version12

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
 const header = document.querySelector(".header");
 const mediaQuery = window.matchMedia('(max-width: 800px)');
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("burger").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("burger").addEventListener("click", function () {
         header.classList.add("open");
     })
-    document.getElementById("burger-close").addEventListener("click", function() {
+    document.getElementById("burger-close").addEventListener("click", function () {
         header.classList.remove("open");
     })
 })
+
+function openOnClick() {
+    document.getElementById("drop-lang").addEventListener("click", function () {
+            document.getElementById("lang-content").classList.toggle("open");
+    })
+}
